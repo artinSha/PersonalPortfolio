16bf015a9f973d080b258b9337e8c54c81290d01
artinSha <artin0610@gmail.com>
2026-05-02 00:34:10 -0700

    Reduced hallway length

--- src/components/scene/SubwayMap.jsx
+++ src/components/scene/SubwayMap.jsx
@@ -9,8 +9,8 @@ const FRAME  = 0.06
 
 // Right wall, in the entry buffer before any project posters
 const WALL_X = SCENE.TUNNEL_WIDTH / 2 - SCENE.POSTER_WALL_OFFSET
-const MAP_Z  = -4
-const MAP_Y  = SCENE.CAMERA_Y + 0.15   // just above eye level center
+const MAP_Z  = -2.5
+const MAP_Y  = SCENE.CAMERA_Y + 0.4   // just above eye level center
 
 function MapMesh() {
   const texture = useTexture('/assets/subway-map.png')
